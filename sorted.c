951aa34e68513be820765f6897850c938203b69b
Yıldırım Akbal <yildirim.akbal@gmail.com>
2021-12-13 15:13:02 +0300

    Add files via upload
    Check wheter a given array is already sorted.

---
+++ sorted.c
@@ -0,0 +1,16 @@
+  
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int sorted(int a[], int length){
+    for(int i = 0; i < length; ++i){
+        if(a[i+1] <= a[i]){
+            printf("Nope Buddy\n");
+            return 0;
+        }
+        } 
+
+    return 1;
+
+}
