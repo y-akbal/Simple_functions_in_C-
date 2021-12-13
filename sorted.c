7df97245c726b492e584407106fdfcedc3bfbb31
Yıldırım Akbal <yildirim.akbal@gmail.com>
2021-12-13 15:16:25 +0300

    Update sorted.c

--- sorted.c
+++ sorted.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int sorted(int a[], int length){
+int sorted_acc(int a[], int length){
     for(int i = 0; i < length; ++i){
         if(a[i+1] <= a[i]){
             printf("Nope Buddy\n");
